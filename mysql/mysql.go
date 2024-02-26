5bf542d2e0f0dc340c18b93de20898706f84f0b7
吴振宇 <942090313@qq.com>
2024-02-26 22:35:59 +0800

    2

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func InitMysql() error {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/zg5?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/zg5?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return err
 }
