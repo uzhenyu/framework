8385df9c7af1c874e673353c0ca80c424f1f6719
吴振宇 <942090313@qq.com>
2024-02-29 09:48:35 +0800

    v.0.2

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -20,7 +20,7 @@ type mysqlConfig struct {
 }
 
 func Listen(serviceName string) *mysqlConfig {
-	_, globalConfig := config.ListenConfig("DEFAULT_GROUP", serviceName)
+	globalConfig, _ := config.ListenConfig("DEFAULT_GROUP", serviceName)
 	logs.Info(globalConfig, 110)
 	if globalConfig != "" {
 		data := &mysqlConfig{}
